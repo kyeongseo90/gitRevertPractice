1110c4423d56a058201086c10ed3f52d4bc3268f
kyeongseo90 <rudtj9099@gmail.com>
2023-09-23 19:40:18 +0900

    version 2

--- src/main/java/com/example/gitrevertpractice/GitRevertPracticeApplication.java
+++ src/main/java/com/example/gitrevertpractice/GitRevertPracticeApplication.java
@@ -8,6 +8,7 @@ public class GitRevertPracticeApplication {
 
 	// version 0
 	// version 1
+	// version 2
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitRevertPracticeApplication.class, args);
