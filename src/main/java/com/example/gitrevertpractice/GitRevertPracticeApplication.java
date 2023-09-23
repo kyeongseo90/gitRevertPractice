a3107bc59eccdaba61abc97314ef0c78f9114ef9
kyeongseo90 <rudtj9099@gmail.com>
2023-09-23 19:27:06 +0900

    version 0

---
+++ src/main/java/com/example/gitrevertpractice/GitRevertPracticeApplication.java
@@ -0,0 +1,15 @@
+package com.example.gitrevertpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRevertPracticeApplication {
+
+	// version 0
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRevertPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitrevertpractice/GitRevertPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitrevertpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRevertPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
