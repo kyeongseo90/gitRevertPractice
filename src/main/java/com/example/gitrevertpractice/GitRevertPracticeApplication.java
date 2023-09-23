1c4e49f5bc2b4c5c9729681eed857bef4d2d05ac
kyeongseo90 <rudtj9099@gmail.com>
2023-09-23 19:29:34 +0900

    version 1

--- src/main/java/com/example/gitrevertpractice/GitRevertPracticeApplication.java
+++ src/main/java/com/example/gitrevertpractice/GitRevertPracticeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitRevertPracticeApplication {
 
 	// version 0
+	// version 1
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitRevertPracticeApplication.class, args);
